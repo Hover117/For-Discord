1ea0013215288fc4c787f86d8a0e87ebf021a51e
Hover117 <hovermaninblack@gmail.com>
2025-02-20 06:20:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+import os
 
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = os.getenv("DISCORD_TOKEN")  # Fetch token from GitHub Secrets
+
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
